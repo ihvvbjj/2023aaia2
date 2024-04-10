40df9c3aed4cc4e2c85090ffdddd02c49729a626
ihvvbjj <12750334@me.mcu.edu.tw>
2024-04-10 12:03:01 +0800

    week08

--- week06/week08/week08-1a.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    for(int i=1; i<=9; i++) {
-        printf("i:%d\n",i);
-
-    }
-}
---
+++ week08/week08-1a.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=1; i<=9; i++) {
+        printf("i:%d\n",i);
+
+    }
+}
