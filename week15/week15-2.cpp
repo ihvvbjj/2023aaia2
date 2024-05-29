56e07cfc7ab48f82ac891cc8ec6bd8527cfc1873
ihvvbjj <12750334@me.mcu.edu.tw>
2024-05-29 10:30:16 +0800

    week15

---
+++ week15/week15-2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    if('A'==65) printf("真的耶! A 就是 65\n");
+    printf("A的值是%d 數字65對應%C\n",'A',65);
+    printf("B的值是%d 數字66對應%C\n",'B',66);
+    printf("C的值是%d 數字67對應%C\n",'C',67);
+    printf("Z的值是%d 數字90對應%C\n",'Z',90);
+    printf("\n");
+    printf("a的值是%d 數字97對應%C\n",'a',97);
+    printf("b的值是%d 數字98對應%C\n",'b',98);
+    printf("c的值是%d 數字99對應%C\n",'c',99);
+    printf("z的值是%d 數字122對應%C\n",'z',122);
+}
